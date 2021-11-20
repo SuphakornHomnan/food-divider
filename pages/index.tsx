6bbe5b47dbc6ae399ece9fce25f5ebe7b25d6b68
job25721 <pathomporn_pankaew@cmu.ac.th>
2021-11-20 22:32:19 +0700

    change flow

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,74 +9,68 @@ import {
   Row,
   Col,
   Form,
+  Nav,
 } from "react-bootstrap";
 import FoodForm from "../src/components/food-form";
 import FoodList, { Member } from "../src/components/food-list";
 import { useModal } from "../src/hooks/use-modal";
 import { useFoodStore } from "../src/hooks/useFoodStore";
 
+enum Navs {
+  foods = "foods",
+  members = "members",
+}
+
 const Home: NextPage = () => {
-  const [{ visible, message }, open, close] = useModal(false);
-  const [name, setName] = useState<string>("");
+  const [active, setActive] = useState<Navs>(Navs.foods);
+  const [openAddMember, closeAddMember, addMemberCtx] = useModal(false);
 
   const { foods, addFoods, members, createMember } = useFoodStore();
 
-  const addInput = (e) => {
-    e.preventDefault();
-    createMember(name);
-    setName("");
+  const onAddFood = () => {
+    addFoods(addMemberCtx.message, 100);
   };
 
-  const onAddFood = () => {
-    addFoods(message, 100);
-  };
+  const renderFoodTab = () => (
+    <>
+      <FoodList foods={foods} />
+      <FoodForm onSubmit={(foodName) => open(foodName)} />
+    </>
+  );
+
+  const renderMemberTab = () => (
+    <>
+    add member
+    </>
+  )
+
   return (
     <Container className="p-3">
-      <Card className="p-3">
+      <Card className="p-3 mb-2">
         <h1>Summary</h1>
         <h2>ราคาอาหารรวม : </h2>
         <h2>จำนวนคน : </h2>
       </Card>
 
-      <FoodList foods={foods} />
-
-      <FoodForm onSubmit={(foodName) => open(foodName)} />
-
-      <Modal show={visible} onHide={close}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add Members</Modal.Title>
-        </Modal.Header>
-        <Modal.Body style={{ padding: 10 }}>
-          <div className="mb-3">
-            {members.map((menber) => (
-              <Member key={menber.id} name={menber.name} />
-            ))}
-          </div>
-          <Form onSubmit={addInput}>
-            <Row>
-              <Col xs={9}>
-                <FormControl
-                  style={{ marginBottom: 10 }}
-                  placeholder="กรอกชื่อ"
-                  value={name}
-                  onChange={(e) => setName(e.target.value as string)}
-                />
-              </Col>
-              <Col>
-                <Button disabled={!name} type="submit">
-                  +
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Modal.Body>
-
-        <Modal.Footer>
-          <Button variant="primary" onClick={onAddFood}>
-            บันทึก
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      <Nav variant="tabs">
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => setActive(Navs.foods)}
+            active={active === Navs.foods}
+          >
+            รายการอาหาร
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link
+            onClick={() => setActive(Navs.members)}
+            active={active === Navs.members}
+          >
+            คนจ่าย
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {active === Navs.foods ? renderFoodTab() : renderMemberTab()}
     </Container>
   );
 };
