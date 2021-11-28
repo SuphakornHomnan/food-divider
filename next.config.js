d73bfb9e20459b035ab3aaf0bec189fcd63e66b9
job25721 <amashiikaze@gmail.com>
2021-11-29 03:25:20 +0700

    update next-pwa config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
 
 module.exports = withPWA({
   reactStrictMode: true,
   pwa: {
-    dest: 'public/sw',
-    sw: '/sw/sw.js'
+    dest: "public/sw",
+    sw: "/sw/sw.js",
+    scope: "/sw",
   },
-})
+  devIndicators: {
+    autoPrerender: false,
+  },
+});
